922ab0d80e2e1bb7001c4e2d04374e9ef1b59987
Simon-GIVRY <simongivry.sg@gmail.com>
2022-11-28 16:02:07 +0100

    projet wordpress ecf

---
+++ Projet-ECF/wp-content/themes/sgs-theme/header-sgs.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+
+</head>
+
+<header>
+    <nav>
+
+        <div>
+            <?php the_custom_logo("Logo_SGS"); ?>
+            <a href="#">Soissons Game Show</a>
+        </div>
+
+        <a href="#">_</a>
+        
+    
+    </nav>
+ 
+</header>
